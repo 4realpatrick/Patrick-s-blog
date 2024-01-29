a5eebe7e648c9e8e2a57e028493b183946f4257f
patrick <patrick@patrickdeMacBook-Pro.local>
2024-01-29 15:19:39 +0800

    feat(auth-layout): make ui responsive

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -1,10 +1,8 @@
 import LoginBackground from "@/components/svg-components/login-background";
-import LoginNavbar from "@/components/login-navbar";
 const AuthLayout = async ({ children }: { children: React.ReactNode }) => {
   return (
     <div className="h-full bg-background">
-      <LoginNavbar />
-      <div className="flex justify-around mt-[100px] mx-8">
+      <div className="flex flex-col-reverse justify-around mt-[100px] mx-8 sm:flex-row ">
         <div className="flex-1 max-w-[800px]">{children}</div>
         <div className="relative ml-10 grow max-w-[1200px]">
           <LoginBackground />
