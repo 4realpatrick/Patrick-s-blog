fa6aa043f22646b44e7d62cf90ef29ab7949b6c9
patrick <patrick@patrickdeMacBook-Pro.local>
2024-01-29 15:19:21 +0800

    feat(auth): add role field and auth callback

--- auth.ts
+++ auth.ts
@@ -1,7 +1,8 @@
-import NextAuth from "next-auth";
+import NextAuth, { type DefaultSession } from "next-auth";
 import { PrismaAdapter } from "@auth/prisma-adapter";
 import authConfig from "@/auth.config";
 import { db } from "./lib/db";
+import { getUserById } from "./data/user";
 
 // https://authjs.dev/guides/upgrade-to-v5#edge-compatibility
 
@@ -11,6 +12,25 @@ export const {
   signIn,
   signOut,
 } = NextAuth({
+  callbacks: {
+    // @ts-ignore offcial bug wait for fix https://github.com/nextauthjs/next-auth/pull/9756/files
+    async session({ token, session }) {
+      if (token.sub && session.user) {
+        session.user.id = token.sub;
+      }
+      if (token.role && session.user) {
+        session.user.role = token.role;
+      }
+      return session;
+    },
+    async jwt({ token }) {
+      if (!token.sub) return token;
+      const existingUser = await getUserById(token.sub);
+      if (!existingUser) return token;
+      token.role = existingUser.role;
+      return token;
+    },
+  },
   adapter: PrismaAdapter(db),
   session: { strategy: "jwt" },
   ...authConfig,
---
+++ next-auth.d.ts
@@ -0,0 +1,9 @@
+import NextAuth, { type DefaultSession } from "next-auth";
+type ExtendedUser = DefaultSession["user"] & {
+  role: Role;
+};
+declare module "next-auth" {
+  interface Session {
+    user: ExtendedUser;
+  }
+}
