9014dd8ba923157d5880f59c2c7f4f13b2a3348b
patrick <patrick@patrickdeMacBook-Pro.local>
2024-02-04 13:13:11 +0800

    fix(separator): border style

--- components/ui/separator.tsx
+++ components/ui/separator.tsx
@@ -3,18 +3,18 @@ import React, { memo } from "react";
 export type TOrientation = "left" | "center" | "right";
 interface ISeparatorProps {
   children?: React.ReactNode;
-  isUsingTheme?: boolean;
+  useTheme?: boolean;
   className?: string;
 }
 const Separator: React.FC<ISeparatorProps> = memo(
-  ({ children, isUsingTheme = true, className }) => {
+  ({ children, useTheme = true, className }) => {
     return (
       <div
         className={cn(
           "flex whitespace-nowrap select-none before:relative before:top-1/2 before:border-t-[1px] before:translate-y-1/2 before:grow after:relative after:top-1/2 after:border-cyan-500 after:border-t-[1px] after:translate-y-1/2 after:grow",
-          isUsingTheme
+          useTheme
             ? "before:border-primary after:border-primary"
-            : "before:border-foreground after:border-foreground",
+            : "before:border-border after:border-border",
           className
         )}
       >
