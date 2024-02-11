87e50649803b852af23b65d02e75860af28e8329
patrick <patrick@patrickdeMacBook-Pro.local>
2024-02-11 23:47:54 +0800

    fix(profile-setting): make cmp completely client side render

--- app/[lang]/setting/_components/profile-setting.tsx
+++ app/[lang]/setting/_components/profile-setting.tsx
@@ -11,9 +11,10 @@ import {
 } from "@/components/dictionary-provider";
 // Hooks
 import { useSession } from "next-auth/react";
-import { useContext } from "react";
+import { useContext, useEffect, useState } from "react";
 
 const ProfileSetting = () => {
+  const [mounted, setMounted] = useState(false);
   const { data } = useSession();
   const locale = useContext(LocaleContext);
   const {
@@ -22,6 +23,11 @@ const ProfileSetting = () => {
     },
     common: commonDictionary,
   } = useContext(DictionaryContext);
+
+  useEffect(() => {
+    setMounted(true);
+  }, []);
+  if (!mounted) return null;
   if (!data?.user) {
     return (
       <div className="size-full flex justify-center items-center flex-col gap-8">
@@ -34,7 +40,6 @@ const ProfileSetting = () => {
     );
   }
   const { user } = data;
-
   return (
     <div className="p-8 space-y-8">
       <h1 className="text-2xl font-bold">{dictionary.title}</h1>
