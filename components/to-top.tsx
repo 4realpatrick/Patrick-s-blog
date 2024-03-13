04b4601c7e49179ed18e28b8a7e2aae63c736f03
patrick <patrick@patrickdeMacBook-Pro.local>
2024-03-13 21:40:38 +0800

    fix(to-top): style change

--- components/to-top.tsx
+++ components/to-top.tsx
@@ -22,15 +22,15 @@ const ToTop = () => {
   return (
     <button
       className={cn(
-        "fixed group items-center justify-start size-11 bg-primary rounded-full cursor-pointer overflow-hidden transition-all duration-200 shadow-lg hover:w-40 hover:rounded-lg active:translate-x-1 active:translate-y-1 bottom-10 right-10 z-10",
+        "fixed group items-center justify-start size-11 bg-primary rounded-full cursor-pointer overflow-hidden transition-all duration-200 shadow-lg hover:rounded-lg bottom-10 right-10 z-10 hover:w-fit hover:pr-4 active:scale-90",
         show ? "flex" : "hidden"
       )}
       onClick={handleToTop}
     >
       <div className="flex items-center justify-center w-full transition-all duration-300 group-hover:justify-start group-hover:px-3">
-        <BiArrowToTop className="size-4 text-background group-hover:scale-105" />
+        <BiArrowToTop className="size-4 text-muted" />
       </div>
-      <div className="absolute right-5 transform translate-x-full opacity-0 text-white text-lg font-semibold transition-all duration-300 group-hover:translate-x-0 group-hover:opacity-100">
+      <div className="absolute opacity-0 text-white text-lg font-semibold transition-all duration-300 group-hover:opacity-100 group-hover:relative whitespace-nowrap">
         {common.to_top}
       </div>
     </button>
