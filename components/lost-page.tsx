afa3faa25e571f95804d6a82a59e5a3298083a65
patrick <patrick@patrickdeMacBook-Pro.local>
2024-03-20 11:29:45 +0800

    feat(lost-page): add animation for rest

--- components/lost-page.tsx
+++ components/lost-page.tsx
@@ -1,10 +1,16 @@
 "use client";
+// Cmp
 import Image404 from "./svg-components/image-404";
-import { useRouter } from "next/navigation";
 import { Button } from "./ui/button";
 import { RiEnglishInput } from "react-icons/ri";
 import { Toggle } from "./ui/toggle";
+// Hooks
+import { useRouter } from "next/navigation";
 import { useMemo, useState } from "react";
+// utils
+import { LazyMotion, domAnimation, m } from "framer-motion";
+import { showup } from "@/constant/animations";
+
 const LostPage = () => {
   const router = useRouter();
   const [enableEn, setEnableEn] = useState(false);
@@ -23,25 +29,34 @@ const LostPage = () => {
   }, [enableEn]);
 
   return (
-    <div className="flex flex-col items-center bg-background px-20 mt-8 justify-center flex-1 space-y-10">
-      <Image404 className="size-[500px]" />
-      <h1 className="text-5xl">{dictionary.title}</h1>
-      <div className="space-x-8 flex items-center">
-        <Toggle
-          aria-label="Toggle language"
-          pressed={enableEn}
-          onPressedChange={setEnableEn}
+    <LazyMotion features={domAnimation}>
+      <div className="flex flex-col items-center bg-background px-20 mt-8 justify-center flex-1 space-y-10">
+        <Image404 className="size-[500px]" />
+        <m.div
+          className="space-y-6"
+          {...showup({
+            delay: 0.5,
+          })}
         >
-          <RiEnglishInput className="size-6" />
-        </Toggle>
-        <Button size="lg" onClick={router.back}>
-          {dictionary.back}
-        </Button>
-        <Button size="lg" onClick={() => router.push("/")}>
-          {dictionary.home}
-        </Button>
+          <h1 className="text-5xl">{dictionary.title}</h1>
+          <div className="space-x-8 flex items-center justify-center">
+            <Toggle
+              aria-label="Toggle language"
+              pressed={enableEn}
+              onPressedChange={setEnableEn}
+            >
+              <RiEnglishInput className="size-6" />
+            </Toggle>
+            <Button size="lg" onClick={router.back}>
+              {dictionary.back}
+            </Button>
+            <Button size="lg" onClick={() => router.push("/")}>
+              {dictionary.home}
+            </Button>
+          </div>
+        </m.div>
       </div>
-    </div>
+    </LazyMotion>
   );
 };
 
