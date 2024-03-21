e25254b67da49c208ae70d8f7d40dbbb5b318d63
patrick <patrick@patrickdeMacBook-Pro.local>
2024-03-21 14:30:38 +0800

    fix(not-found): add not-found.tsx

---
+++ app/not-found.tsx
@@ -0,0 +1,11 @@
+import LostPage from "@/components/lost-page";
+
+const NotFound = () => {
+  return (
+    <div className="flex flex-col h-full">
+      <LostPage />
+    </div>
+  );
+};
+
+export default NotFound;
