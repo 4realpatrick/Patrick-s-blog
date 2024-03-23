fe00546dbd8037a7a97dfd3359e01e9e6db11f24
patrick <patrick@patrickdeMacBook-Pro.local>
2024-03-23 20:03:56 +0800

    fix: remove unused file

--- app/[lang]/article/[id]/layout.tsx
+++
@@ -1,5 +0,0 @@
-const ArticleLayout = ({ children }: { children: React.ReactNode }) => {
-  return <div className="size-full p-10 flex flex-col pt-20">{children}</div>;
-};
-
-export default ArticleLayout;
--- app/[lang]/article/[id]/page.tsx
+++
@@ -1,7 +0,0 @@
-export default function ArticlePage({
-  params: { id },
-}: {
-  params: { id: string };
-}) {
-  return <div className="card">{id}</div>;
-}
