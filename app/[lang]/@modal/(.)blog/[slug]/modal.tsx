d674687641c9a16732a2791dd3a05ef4b3d2393a
patrick <patrick@patrickdeMacBook-Pro.local>
2024-03-25 15:28:15 +0800

    feat(blog): finish modal mode

--- app/[lang]/@modal/(.)blog/[slug]/modal.tsx
+++ app/[lang]/@modal/(.)blog/[slug]/modal.tsx
@@ -1,15 +1,21 @@
 "use client";
-
-import { type ElementRef, useEffect, useRef } from "react";
-import { useRouter } from "next/navigation";
-import { createPortal } from "react-dom";
+// Cmp
 import { Button } from "@/components/ui/button";
 import { BsArrowsFullscreen } from "react-icons/bs";
-import { IoIosCloseCircle } from "react-icons/io";
+import { FaBackspace } from "react-icons/fa";
+import Hint from "@/components/hint";
+// Hooks
+import { type ElementRef, useContext, useEffect, useRef } from "react";
+import { useRouter } from "next/navigation";
+// Utils
+import { createPortal } from "react-dom";
+// Context
+import { DictionaryContext } from "@/components/dictionary-provider";
+
 export function Modal({ children }: { children: React.ReactNode }) {
   const router = useRouter();
   const dialogRef = useRef<ElementRef<"dialog">>(null);
-
+  const { common: commonDictionary } = useContext(DictionaryContext);
   useEffect(() => {
     if (!dialogRef.current?.open) {
       dialogRef.current?.showModal();
@@ -21,25 +27,30 @@ export function Modal({ children }: { children: React.ReactNode }) {
   }
 
   return createPortal(
-    <div className="absolute inset-0 bg-black/50 backdrop-blur flex items-center justify-center z-[1000]">
+    <div className="absolute inset-0 bg-foreground/50 backdrop-blur flex items-center justify-center z-[1000]">
       <dialog
         ref={dialogRef}
-        className="w-4/5 max-h-[80%] h-4/5 bg-background p-5 rounded-md"
+        className="container bg-background p-5 rounded-md scroll-smooth"
         onClose={onDismiss}
       >
-        <header className="flex gap-2 justify-end">
-          <Button
-            onClick={() => location.reload()}
-            variant="ghost"
-            className=""
-          >
-            <BsArrowsFullscreen className="text-green-500" />
-          </Button>
-          <Button onClick={onDismiss} variant="ghost" className="">
-            <IoIosCloseCircle className="text-destructive size-full" />
-          </Button>
+        <header className="flex gap-2">
+          <Hint descrption={commonDictionary.go_back}>
+            <Button
+              className="group"
+              variant="ghost"
+              size="sm"
+              onClick={onDismiss}
+            >
+              <FaBackspace className="size-4 group-hover:text-destructive" />
+            </Button>
+          </Hint>
+          <Hint descrption={commonDictionary.full_screen}>
+            <Button onClick={() => location.reload()} variant="ghost" size="sm">
+              <BsArrowsFullscreen className="text-green-500" />
+            </Button>
+          </Hint>
         </header>
-        <div>{children}</div>
+        {children}
       </dialog>
     </div>,
     document.getElementById("modal-root")!
--- app/[lang]/@modal/(.)blog/[slug]/page.tsx
+++ app/[lang]/@modal/(.)blog/[slug]/page.tsx
@@ -1,15 +1,38 @@
-import { posts } from "#site/content";
-import { Metadata } from "next";
+// Cmp
 import { Modal } from "./modal";
-import {
-  getPostFromParams,
-  IBlogDetailPageProps,
-} from "@/app/[lang]/blog/[...slug]/page";
+import { Badge } from "@/components/ui/badge";
+import { MDXContent } from "@/components/mdx-content";
+import { BlogFooter } from "@/app/[lang]/blog/[...slug]/_components/footer";
+import { FaHashtag } from "react-icons/fa";
+import { FaCalendar } from "react-icons/fa";
+// Constant
+import { posts } from "#site/content";
 import { siteMetadata } from "@/data/site";
+// Types
+import { Metadata } from "next";
+// Utils
+import { getLocaleFromUrl } from "@/lib/get-locale";
+import { notFound } from "next/navigation";
+import dayjs from "dayjs";
+import { getFormatterByLocale } from "@/lib/time";
+
+interface IBlogModalPageProps {
+  params: {
+    slug: string;
+  };
+}
+
+export async function getPostFromParams(params: IBlogModalPageProps["params"]) {
+  const slug = Array.isArray(params.slug)
+    ? params?.slug?.join("/")
+    : params.slug;
+  const blog = posts.find((post) => post.slugAsParams === slug);
+  return blog;
+}
 
 export async function generateMetadata({
   params,
-}: IBlogDetailPageProps): Promise<Metadata> {
+}: IBlogModalPageProps): Promise<Metadata> {
   const blog = await getPostFromParams(params);
 
   if (!blog) {
@@ -47,15 +70,55 @@ export async function generateMetadata({
 }
 
 export async function generateStaticParams(): Promise<
-  IBlogDetailPageProps["params"][]
+  IBlogModalPageProps["params"][]
 > {
-  return posts.map((post) => ({ slug: post.slugAsParams.split("/") }));
+  return posts.map((post) => ({ slug: post.slugAsParams }));
 }
 
-export default function ArticleModal({
-  params: { id: articleId },
-}: {
-  params: { id: string };
-}) {
-  return <Modal>{articleId}</Modal>;
+export default async function ArticleModal({ params }: IBlogModalPageProps) {
+  const locale = getLocaleFromUrl();
+
+  const blog = await getPostFromParams(params);
+
+  if (!blog) {
+    return notFound();
+  }
+  return (
+    <Modal>
+      <div className="container mx-auto py-10 space-y-4">
+        <article className="!container prose dark:prose-invert mx-auto py-10 bg-background px-8 border rounded-md space-y-4">
+          <h1>{blog.title}</h1>
+          <div className="flex flex-wrap items-center gap-2">
+            {blog.tags?.map((tag) => (
+              <Badge className="py-2 px-4" variant="secondary" key={tag}>
+                <FaHashtag className="mr-2 size-4 hidden lg:inline" />
+                {tag}
+              </Badge>
+            ))}
+          </div>
+          <div className="flex items-center gap-2">
+            <FaCalendar />
+            {dayjs(blog.date)
+              .locale(locale)
+              .format(getFormatterByLocale(locale))}
+          </div>
+          <hr className="my-4" />
+          {blog.description ? (
+            <blockquote className="py-6 border-l-2 pl-6 italic">
+              {blog.description}
+            </blockquote>
+          ) : null}
+          <MDXContent code={blog.body} />
+          <div className="pl-4">
+            <hr />
+            <BlogFooter slug={params.slug} />
+          </div>
+        </article>
+        <div className="space-y-4">
+          <h2 className="text-3xl font-bold mb-4">Comment</h2>
+          <div className="border-2 text-xl rounded-md p-4">coming soon...</div>
+        </div>
+      </div>
+    </Modal>
+  );
 }
