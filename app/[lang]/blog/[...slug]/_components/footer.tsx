bccd0f13dcbdd947268176fed2c96868084f1035
patrick <patrick@patrickdeMacBook-Pro.local>
2024-03-25 15:28:34 +0800

    feat(blog): footer props type change

--- app/[lang]/blog/[...slug]/_components/footer.tsx
+++ app/[lang]/blog/[...slug]/_components/footer.tsx
@@ -1,12 +1,8 @@
 "use client";
 // Hooks
-import { useBlogState } from "@/hooks/use-blog-state";
 import { useRouter } from "next/navigation";
 
-export const BlogFooter = ({ slug }: { slug: string[] }) => {
-  const blogs = useBlogState((state) => state.blogs);
-  const realSlug = Array.isArray(slug) ? slug?.join("/") : slug;
-  const curBlogIndex = blogs.findIndex((blog) => blog.slug === realSlug);
+export const BlogFooter = ({ slug }: { slug: string[] | string }) => {
   const router = useRouter();
   return (
     <div className="mx-auto flex items-center justify-between">
