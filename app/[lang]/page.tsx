6c1a4d9e46621b70b09c8cbac6fba1bfbeef5317
patrick <patrick@patrickdeMacBook-Pro.local>
2024-03-25 15:45:20 +0800

    feat(page): last things

--- app/[lang]/page.tsx
+++ app/[lang]/page.tsx
@@ -1,19 +1,10 @@
 "use client";
 // Cmp
 import Home from "@/components/home";
-// Hooks
-import { useContext, useRef } from "react";
-// Context
-import { DictionaryContext } from "@/components/dictionary-provider";
 // Utils
-import { LazyMotion, domAnimation, m } from "framer-motion";
+import { LazyMotion, domAnimation } from "framer-motion";
 
 export default function HomePage() {
-  const {
-    pages: { home: dictionary },
-    common: commonDictionary,
-  } = useContext(DictionaryContext);
-  const avatarRef = useRef();
   return (
     <LazyMotion features={domAnimation}>
       <Home />
--- components/home/index.tsx
+++ components/home/index.tsx
@@ -1,25 +1,11 @@
 import Intro from "./intro";
 import Habbit from "./habbit";
-import Link from "next/link";
-const Home = () => {
-  let photos = Array.from({ length: 6 }, (_, i) => i + 1);
 
+const Home = () => {
   return (
     <>
       <Intro />
       <Habbit />
-      <div className="flex gap-4">
-        {photos.map((id) => (
-          <Link
-            className="size-10 bg-primary"
-            key={id}
-            href={`/article/${id}`}
-            passHref
-          >
-            {id}
-          </Link>
-        ))}
-      </div>
     </>
   );
 };
